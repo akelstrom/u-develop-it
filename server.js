89d33d43777c7fea020cdbb5bbf2a07cf3ac20ff
akelstrom <akelstrom@gmail.com>
2021-02-02 13:43:22 -0500

    voters done and moduarlize done

--- server.js
+++ server.js
@@ -1,29 +1,24 @@
-//dependecies
-const express = require("express");
+const express = require('express');
 const db = require('./db/database');
-const inputCheck = require("./utils/inputCheck");
-const apiRoutes = require('./routes/apiRoutes');
 
-
-//server
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+const apiRoutes = require('./routes/apiRoutes');
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-//use api routes
+// Use apiRoutes
 app.use('/api', apiRoutes);
 
-
-// Default response for any other request(Not Found) Catch all
+// Default response for any other request(Not Found) Catch all other
 app.use((req, res) => {
   res.status(404).end();
 });
 
 // Start server after DB connection
-db.on("open", () => {
+db.on('open', () => {
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
   });
